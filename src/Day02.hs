11b34043b3d01dccfc658620e2d03e133829f578
Tom Jemmett <thomas.jemmett@nhs.net>
2025-12-03 16:03:53 +0000

    rewrite day 2 for performance

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,11 +2,9 @@ module Day02 where
 
 import AOCSolution (getSolution)
 import Common
-import Data.List (splitAt)
-import Data.List.Split (chunksOf)
+import Data.List (nub)
 import Inputs (InputType (..), readInput)
 import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
 
 day02SampleInput, day02ActualInput :: IO (Maybe String)
 day02SampleInput = readInput Sample 2
@@ -21,25 +19,23 @@ day02 = do
 solve :: String -> (String, String)
 solve = getSolution parseInput part1 part2
 
-part :: (Int -> Bool) -> [(Int, Int)] -> Int
-part isInvalid = sum . map fn
-  where
-    fn (s, e) = sum $ filter isInvalid [s .. e]
-
 part1 :: [(Int, Int)] -> Int
-part1 = part isInvalid
-  where
-    isInvalid (show -> s) = stringRepeatsN s ((length s + 1) `div` 2)
+part1 = sum . map (findRepeats $ take 1)
 
 part2 :: [(Int, Int)] -> Int
-part2 = part isInvalid
-  where
-    isInvalid (show -> s) = any (stringRepeatsN s) [1 .. length s `div` 2]
-
-stringRepeatsN :: String -> Int -> Bool
-stringRepeatsN s n = length xs > 1 && all (== head xs) (tail xs)
-  where
-    xs = chunksOf n s
+part2 = sum . map (findRepeats id)
 
 parseInput :: String -> [(Int, Int)]
 parseInput = parse (((,) <$> (number <* P.char '-') <*> number) `P.sepBy` P.char ',')
+
+findRepeats :: ([Int] -> [Int]) -> (Int, Int) -> Int
+findRepeats dfn (lo, hi) = sum . nub . concat $ do
+  let digits = floor (logBase 10 (fromIntegral hi)) + 1
+  d <- [1 .. digits]
+  r <- dfn [2 .. (digits `div` d) + 1]
+  let p10d = 10 ^ d
+      p10dr = 10 ^ (d * r)
+      f = (p10dr - 1) `div` (p10d - 1)
+      k_lo = max ((lo + f - 1) `div` f) (10 ^ (d - 1))
+      k_hi = min (hi `div` f) (p10d - 1)
+  pure $ filter ((>= lo) <&> (<= hi)) $ map (* f) [k_lo .. k_hi]

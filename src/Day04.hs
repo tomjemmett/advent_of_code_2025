5af6c37c504ca91d32d1290e3607178a747ce1c0
Tom Jemmett <thomas.jemmett@nhs.net>
2025-12-04 09:16:43 +0000

    adds day 4 solution

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,9 +2,9 @@ module Day04 where
 
 import AOCSolution (getSolution)
 import Common
+import Control.Monad (guard)
+import Data.HashSet qualified as S
 import Inputs (InputType (..), readInput)
-import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
 
 day04SampleInput, day04ActualInput :: IO (Maybe String)
 day04SampleInput = readInput Sample 4
@@ -17,4 +17,31 @@ day04 = do
   return (s, a)
 
 solve :: String -> (String, String)
-solve = undefined
+solve = getSolution parseInput part1 part2
+
+parseInput :: String -> S.HashSet Point2d
+parseInput input = S.fromList do
+  (i, line) <- zip [0 ..] $ lines input
+  (j, v) <- zip [0 ..] line
+  guard $ v == '@'
+  pure (i, j)
+
+canRemove :: S.HashSet Point2d -> S.HashSet Point2d
+canRemove points = S.filter ((< 4) . countNeighbors) points
+  where
+    countNeighbors :: Point2d -> Int
+    countNeighbors = length . filter (`S.member` points) . point2dNeighboursDiags
+
+part1 :: S.HashSet Point2d -> Int
+part1 = S.size . canRemove
+
+part2 :: S.HashSet Point2d -> Int
+part2 = go
+  where
+    go p =
+      if S.null rm
+        then 0
+        else S.size rm + go p'
+      where
+        rm = canRemove p
+        p' = S.difference p rm
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -7,7 +7,7 @@ spec = describe "Day 4" $ do
   (sample, actual) <- runIO day04
 
   it "Sample" $ do
-    sample `shouldBe` ("", "")
+    sample `shouldBe` ("13", "1457")
 
   it "Actual" $ do
-    actual `shouldBe` ("", "")
+    actual `shouldBe` ("43", "8310")

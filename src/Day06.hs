80a95211409a01f1d173b31b6d834ffa35ee9315
Tom Jemmett <thomas.jemmett@nhs.net>
2025-12-06 10:10:33 +0000

    adds day 6 solution

--- src/Day06.hs
+++ src/Day06.hs
@@ -2,9 +2,12 @@ module Day06 where
 
 import AOCSolution (getSolution)
 import Common
+import Data.Bifunctor (second)
+import Data.List (transpose)
+import Data.List.Split (chunksOf)
 import Inputs (InputType (..), readInput)
-import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
+
+data Op = Multiply | Add deriving (Show)
 
 day06SampleInput, day06ActualInput :: IO (Maybe String)
 day06SampleInput = readInput Sample 6
@@ -17,4 +20,45 @@ day06 = do
   return (s, a)
 
 solve :: String -> (String, String)
-solve = undefined
+solve = getSolution parseInput part1 part2
+
+parseInput :: String -> [(Op, [String])]
+parseInput input = parseOp (init i) (last i ++ " ")
+  where
+    i = lines input
+
+parseOp :: [String] -> String -> [(Op, [String])]
+parseOp _ [] = []
+parseOp nums (o : ops) = (op, ns) : parseOp nums' ops'
+  where
+    n = length $ takeWhile (== ' ') ops
+    op = getOp [o]
+    ops' = drop n ops
+    nums' = map (drop $ succ n) nums
+    ns = map (take n) nums
+
+part1 :: [(Op, [String])] -> Int
+part1 = solution . map (second f)
+  where
+    f = map (read . filter (/= ' '))
+
+part2 :: [(Op, [String])] -> Int
+part2 = solution . map (second makeNums)
+  where
+    makeNums :: [String] -> [Int]
+    makeNums xs | all (== "") xs = []
+    makeNums xs = i : makeNums (map tail xs)
+      where
+        i = read $ filter (/= ' ') $ map head xs
+
+runOp :: Op -> [Int] -> Int
+runOp Multiply = product
+runOp Add = sum
+
+solution :: [(Op, [Int])] -> Int
+solution = sum . map (uncurry runOp)
+
+getOp :: String -> Op
+getOp = \case
+  "*" -> Multiply
+  "+" -> Add
--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -7,7 +7,7 @@ spec = describe "Day 6" $ do
   (sample, actual) <- runIO day06
 
   it "Sample" $ do
-    sample `shouldBe` ("", "")
+    sample `shouldBe` ("4277556", "3263827")
 
   it "Actual" $ do
-    actual `shouldBe` ("", "")
+    actual `shouldBe` ("6635273135233", "12542543681221")

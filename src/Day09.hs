8853d31faf0336687927dab3fc0c1b225e267d63
Tom Jemmett <thomas.jemmett@nhs.net>
2025-12-09 09:23:25 +0000

    adds day 9 solution

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,9 +2,19 @@ module Day09 where
 
 import AOCSolution (getSolution)
 import Common
+import Data.Function (on)
+import Data.List (find, sortBy)
+import Data.Maybe (fromJust)
 import Inputs (InputType (..), readInput)
-import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
+
+data Input = Input
+  { path :: [Point2d],
+    rectangles :: [((Point2d, Point2d), Int)]
+  }
+  deriving (Show)
+
+mkInput :: [Point2d] -> Input
+mkInput pts = Input (pts ++ take 1 pts) (getAllRectangles pts)
 
 day09SampleInput, day09ActualInput :: IO (Maybe String)
 day09SampleInput = readInput Sample 9
@@ -17,4 +27,42 @@ day09 = do
   return (s, a)
 
 solve :: String -> (String, String)
-solve = undefined
+solve = getSolution parseInput part1 part2
+
+parseInput :: String -> Input
+parseInput = mkInput . map (tuplify2 . commaSeparatedInts) . lines
+
+getAllRectangles :: [Point2d] -> [((Point2d, Point2d), Int)]
+getAllRectangles points =
+  sortBy
+    (flip compare `on` snd)
+    [ ((p1, p2), getRectangleAreas p1 p2)
+      | p1 <- points,
+        p2 <- points,
+        p1 < p2
+    ]
+
+getRectangleAreas :: Point2d -> Point2d -> Int
+getRectangleAreas (x1, y1) (x2, y2) = (abs (x2 - x1) + 1) * (abs (y2 - y1) + 1)
+
+part1 :: Input -> Int
+part1 = snd . head . rectangles
+
+part2 :: Input -> Int
+part2 (Input {..}) = snd $ fromJust $ find (insidePath path) rectangles
+
+insidePath :: [Point2d] -> ((Point2d, Point2d), Int) -> Bool
+insidePath path (rect, _) = all checkLine (zip path (tail path))
+  where
+    (rectXmin, rectXmax, rectYmin, rectYmax) = f rect
+    checkLine (f -> (lineXmin, lineXmax, lineYmin, lineYmax)) =
+      or
+        [ lineXmax <= rectXmin,
+          rectXmax <= lineXmin,
+          lineYmax <= rectYmin,
+          rectYmax <= lineYmin
+        ]
+    f ((x1, y1), (x2, y2)) =
+      let (xmin, xmax) = if x1 < x2 then (x1, x2) else (x2, x1)
+          (ymin, ymax) = if y1 < y2 then (y1, y2) else (y2, y1)
+       in (xmin, xmax, ymin, ymax)
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -7,7 +7,7 @@ spec = describe "Day 9" $ do
   (sample, actual) <- runIO day09
 
   it "Sample" $ do
-    sample `shouldBe` ("", "")
+    sample `shouldBe` ("50", "24")
 
   it "Actual" $ do
-    actual `shouldBe` ("", "")
+    actual `shouldBe` ("4777409595", "1473551379")

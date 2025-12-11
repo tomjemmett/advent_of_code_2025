d462088ac8b96afcd6654de95f9989152efd6c29
Tom Jemmett <thomas.jemmett@nhs.net>
2025-12-11 07:44:32 +0000

    adds day 11 solution

--- src/Day11.hs
+++ src/Day11.hs
@@ -2,6 +2,9 @@ module Day11 where
 
 import AOCSolution (getSolution)
 import Common
+import Control.Monad (forM)
+import Control.Monad.State
+import Data.HashMap.Strict qualified as M
 import Inputs (InputType (..), readInput)
 import Text.Parsec qualified as P
 import Text.Parsec.String (Parser)
@@ -17,4 +20,41 @@ day11 = do
   return (s, a)
 
 solve :: String -> (String, String)
-solve = undefined
+solve = getSolution parseInput part1 part2
+
+parseInput :: String -> M.HashMap String [String]
+parseInput = M.fromList . parse (pLine `P.sepEndBy` P.newline)
+  where
+    pLine :: Parser (String, [String])
+    pLine = do
+      from <- pName
+      P.string ": "
+      to <- pName `P.sepBy` P.char ' '
+      pure (from, to)
+    pName :: Parser String
+    pName = P.many P.alphaNum
+
+part1 :: M.HashMap String [String] -> Int
+part1 i = go "you"
+  where
+    go "out" = 1
+    go p = sum $ map go $ i M.! p
+
+part2 :: M.HashMap String [String] -> Int
+part2 i = evalState (go 2 "svr") M.empty
+  where
+    go :: Int -> String -> State (M.HashMap (Int, String) Int) Int
+    go v "out" = pure $ fromEnum (v == 0)
+    go v p = do
+      s <- get
+      case s M.!? (v, p) of
+        Just n -> pure n
+        Nothing -> do
+          let ps = i M.! p
+              v' = case p of
+                "dac" -> v - 1
+                "fft" -> v - 1
+                _ -> v
+          n <- sum <$> forM ps (go v')
+          modify (M.insert (v, p) n)
+          pure n
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -7,7 +7,7 @@ spec = describe "Day 11" $ do
   (sample, actual) <- runIO day11
 
   it "Sample" $ do
-    sample `shouldBe` ("", "")
+    sample `shouldBe` ("5", "2")
 
   it "Actual" $ do
-    actual `shouldBe` ("", "")
+    actual `shouldBe` ("428", "331468292364745")

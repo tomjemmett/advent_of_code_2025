d51fd4229c13e934a931c09281be6d4d32280fdf
Tom Jemmett <thomas.jemmett@nhs.net>
2026-04-06 21:57:39 +0100

    adds solution for day 7

--- src/day07.rs
+++ src/day07.rs
@@ -1,10 +1,54 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
 
+struct State {
+    grid: Vec<Vec<char>>,
+    visited: HashSet<(usize, usize)>,
+    cache: HashMap<(usize, usize), u128>,
+    end: usize,
+}
+
 pub fn run(file_type: &str) -> Option<(String, String)> {
-    if let Ok(_contents) = fs::read_to_string(format!("inputs/{}/07.txt", file_type)) {
-        // let input = &parse_input(&contents);
-        panic!("Not implemented");
+    if let Ok(contents) = fs::read_to_string(format!("inputs/{}/07.txt", file_type)) {
+        let input = contents.lines().collect::<Vec<&str>>();
+
+        let start = input[0].find('S').unwrap();
+        let mut state = State {
+            grid: input.iter().map(|line| line.chars().collect()).collect(),
+            visited: HashSet::new(),
+            cache: HashMap::new(),
+            end: input.len(),
+        };
+
+        let result = move_beam(start, 0, &mut state);
+
+        Some((state.visited.len().to_string(), result.to_string()))
     } else {
         None
     }
+}
+
+fn move_beam(x: usize, y: usize, state: &mut State) -> u128 {
+    if let Some(cache_val) = state.cache.get(&(x, y)) {
+        return *cache_val;
+    }
+
+    if y == state.end {
+        return 1;
+    }
+
+    let result = if state.grid[y][x] == '.' {
+        move_beam(x, y + 1, state)
+    } else {
+        state.visited.insert((x, y));
+        [x - 1, x + 1]
+            .iter()
+            .map(|dx| move_beam(*dx, y + 1, state))
+            .sum()
+    };
+
+    state.cache.insert((x, y), result);
+
+    result
 }
